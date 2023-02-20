91f4ad6b55c7d5c4bb2fb6800998ae2994500e62
gcarolina <karolinaganizoda@gmail.com>
2023-02-21 00:32:52 +0300

    Project created

---
+++ TimerSwiftUIHW/TimerSwiftUIHW/TimerSwiftUIHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerSwiftUIHWApp.swift
+//  TimerSwiftUIHW
+//
+//  Created by Carolina on 21.02.23.
+//
+
+import SwiftUI
+
+@main
+struct TimerSwiftUIHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
